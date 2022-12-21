4dc7fafbc14cfdacec54f0b0ce876994812e19ad
godcandidate <godcandidate@gmail.com>
2022-12-21 07:54:27 +0000

    update shell_list1.c

--- shell_list1.c
+++ shell_list1.c
@@ -9,7 +9,7 @@
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h)
 	{
 		h = h->next;
@@ -30,7 +30,7 @@ char **list_to_strings(list_t *head)
 	size_t i = list_len(head), j;
 	char **strs;
 	char *str;
-	
+
 	if (!head || !i)
 		return (NULL);
 	strs = malloc(sizeof(char *) * (i + 1));
@@ -46,12 +46,14 @@ char **list_to_strings(list_t *head)
 			free(strs);
 			return (NULL);
 		}
-		
+
 		str = _strcpy(str, node->str);
 		strs[i] = str;
 	}
-	strs[i] = NULL;									return (strs);
+	strs[i] = NULL;
+	return (strs);
 }
+
 
 /**
  * print_list - prints all elements of a list_t linked list
@@ -62,19 +64,19 @@ char **list_to_strings(list_t *head)
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
+
 	while (h)
 	{
 		_puts(convert_number(h->num, 10, 0));
-		_putchar(':');									_putchar(' ');
+		_putchar(':');
+		_putchar(' ');
 		_puts(h->str ? h->str : "(nil)");
 		_puts("\n");
 		h = h->next;
 		i++;
 	}
 	return (i);
-
 }
-
 
 /**
  * node_starts_with - returns node whose string starts with prefix
@@ -87,7 +89,7 @@ size_t print_list(const list_t *h)
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
 	char *p = NULL;
-	
+
 	while (node)
 	{
 		p = starts_with(node->str, prefix);
@@ -108,6 +110,7 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 ssize_t get_node_index(list_t *head, list_t *node)
 {
 	size_t i = 0;
+
 	while (head)
 	{
 		if (head == node)
